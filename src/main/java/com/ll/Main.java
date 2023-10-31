0409eb790d074f5eeb0e36f2399458bbe90441cf
Sanghyuk Park <dropthegit@gmail.com>
2023-10-31 09:32:20 +0900

    2번

--- src/main/java/com/ll/Main.java
+++ src/main/java/com/ll/Main.java
@@ -4,5 +4,7 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("1번 PR");
         System.out.println("2번 PR");
+        System.out.println("2번 PR");
+
     }
 }
